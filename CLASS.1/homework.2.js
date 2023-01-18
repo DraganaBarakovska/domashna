73fa1cc4ba9e735eda79dd35e99dd6c064feff72
DraganaBarakovska <barakovskadragana67@gmail.com>
2023-01-18 19:30:03 +0100

    Class1 Homework

---
+++ CLASS.1/homework.2.js
@@ -0,0 +1,5 @@
+var field = 0;
+function addText(Dragana, Aleksandra, Jana, Ana, Eva){
+  if(field === 0) return false;
+  field.value = Dragana, Aleksandra, Jana, Ana, Eva;
+}
---
+++ CLASS.1/homework.4.js
@@ -0,0 +1,15 @@
+let inputs = document.getElementById("student-form").children;
+let result = document.getElementById("result");
+let submitBtn = document.getElementById("submit");
+
+function getUserInfo(inputElements){
+let result = "User: ";
+for (let input of inputElements) {
+	result += input.value + " ";
+}
+result += "is registered!"
+return result;
+}
+submitBtn.addEventListener("click", function(){
+result.innerText = getUserInfo(inputs);
+})
