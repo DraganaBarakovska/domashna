62a8b4bc1d93f1263204f433a2ca6ba763c0136f
DraganaBarakovska <barakovskadragana67@gmail.com>
2023-03-08 23:10:14 +0100

    'Homework2'

---
+++ SumOfEven/StudentGroup/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string[] studentsG1 = new string[] { "Dragana" , "Aleksandra", "Martina", "Eva", "Kristina" };
+string[] studentsG2 = new string[] { "Mario" , "Kristijan", "Petar", "Tomi", "Branko"};
+
+Console.WriteLine("Enter 1 or 2");
+string studentsArray = Console.ReadLine();
+
+if (studentsArray == "1")
+{
+   Console.WriteLine($"{string.Join(", ", studentsG1)}");
+}
+else if (studentsArray == "2")
+{
+    Console.WriteLine($"{string.Join(", ", studentsG2)}");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+
+
+
+
+
---
+++ SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,27 @@
+﻿int i = 0;
+int[] arr = new int[6];
+int even = 0;
+
+
+Console.WriteLine("Enter array numbers : ");
+for (i = 0; i < arr.Length; i++)
+{
+    Console.Write("Number[" + (i + 1) + "]: ");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+
+
+Console.WriteLine("List of even  numbers : ");
+for (i = 0; i < arr.Length; i++)
+{
+    
+    if (arr[i] % 2 == 0)
+        even += arr[i];
+    Console.WriteLine(even);
+
+
+}
+Console.WriteLine();
+
+
+
